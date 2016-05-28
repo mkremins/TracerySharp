8b654f4997576984ed656b34ad40701daad87c0d
Max Kreminski <maxkreminski@gmail.com>
2016-05-28 17:47:30 -0400

    Cleanup: remove unused imports

--- Source/Unity/GrammarEditor.cs
+++ Source/Unity/GrammarEditor.cs
@@ -1,7 +1,5 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEditor;
+﻿using UnityEditor;
+using UnityEngine;
 using Tracery;
 
 [CustomEditor(typeof(TraceryGrammar))]
