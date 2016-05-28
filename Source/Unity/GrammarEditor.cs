b53fa495586e1674e2c411d8c113a5ffc3ce31d5
Max Kreminski <maxkreminski@gmail.com>
2016-05-28 17:22:51 -0400

    Preliminary Unity editor integration

---
+++ Source/Unity/GrammarEditor.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using Tracery;
+
+[CustomEditor(typeof(TraceryGrammar))]
+public class GrammarEditor : Editor
+{
+	private string testValue = "";
+
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+
+		SerializedProperty symbols = serializedObject.FindProperty("symbols");
+
+		for (int i = 0; i < symbols.arraySize; i++)
+		{
+			SerializedProperty symbol = symbols.GetArrayElementAtIndex(i);
+			SerializedProperty key = symbol.FindPropertyRelative("key");
+			SerializedProperty rules = symbol.FindPropertyRelative("rules");
+
+			GUILayout.BeginHorizontal();
+			key.stringValue = GUILayout.TextField(key.stringValue, GUILayout.Width(120f));
+			bool remove = GUILayout.Button("Remove", EditorStyles.miniButton, GUILayout.Width(60f));
+			GUILayout.EndHorizontal();
+
+			if (remove)
+			{
+				symbols.DeleteArrayElementAtIndex(i);
+			}
+			else
+			{
+				rules.stringValue = GUILayout.TextArea(rules.stringValue);
+			}
+
+			GUILayout.Space(5);
+		}
+
+		if (GUILayout.Button("Add Rule"))
+		{
+			symbols.InsertArrayElementAtIndex(symbols.arraySize);
+
+			// set default values
+			SerializedProperty symbol = symbols.GetArrayElementAtIndex(symbols.arraySize - 1);
+			SerializedProperty key = symbol.FindPropertyRelative("key");
+			SerializedProperty rules = symbol.FindPropertyRelative("rules");
+			key.stringValue = "ruleName";
+			rules.stringValue = "rules";
+		}
+
+		GUILayout.Space(5);
+
+		if (GUILayout.Button("Test Grammar"))
+		{
+			Grammar grammar = ((TraceryGrammar)target).Grammar;
+			testValue = grammar.Flatten("#origin#");
+		}
+
+		GUILayout.Label(testValue);
+
+		serializedObject.ApplyModifiedProperties();
+	}
+}
---
+++ Source/Unity/TraceryGrammar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Tracery;
+
+[System.Serializable]
+public class TraceryGrammar : MonoBehaviour
+{
+	public Symbol[] symbols;
+
+	public Grammar Grammar
+	{
+		get
+		{
+			Grammar grammar = new Grammar();
+			foreach (Symbol s in symbols)
+			{
+				grammar.PushRules(s.key, s.rules.Split('\n'));
+			}
+			return grammar;
+		}
+	}
+
+	[System.Serializable]
+	public class Symbol
+	{
+		public string key;
+		public string rules;
+	}
+}
